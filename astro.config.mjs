05cdb0c5d516c127aa2a44fac5520402cad3f95b
TheBharatMishra <mishrabm2004@gmail.com>
2023-09-10 23:42:53 +0530

    Changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://parallelintersect.github.io",
-  base: "/parallelintersect.github.io",
+  //   base: "/parallelintersect.github.io",
 });
