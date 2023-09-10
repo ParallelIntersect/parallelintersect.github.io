7277164e925e6b9d4c8b6cafd62e7346e7735fb4
TheBharatMishra <mishrabm2004@gmail.com>
2023-09-11 02:26:03 +0530

    Made it mine

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from "astro/config";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://parallelintersect.github.io",
+  site: "https://parallelintersect.github.io"
   //   base: "/parallelintersect.github.io",
+  ,
+  integrations: [tailwind()]
 });
